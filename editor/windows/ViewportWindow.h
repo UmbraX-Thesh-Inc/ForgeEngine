5907f7d16d03d82177ee342f319b38232edb14c1
UmbraX Tesh Inc <furia0322@gmail.com>
2026-03-04 01:14:53 -0500

    Subida automática: editor/windows/ViewportWindow.h

---
+++ editor/windows/ViewportWindow.h
@@ -0,0 +1,6 @@
+#pragma once
+// ============================================================
+//  ForgeEngine – ViewportWindow.h
+// ============================================================
+void RenderViewport3D();
+void RenderViewport2D();
